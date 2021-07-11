e777e773a3d4876d80b40c7e39a1ce5837044cc6
Prikusator <squonchi@gmail.com>
2021-07-12 01:33:29 +0300

    Refactor + adding tests

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -16,18 +17,27 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Запуск основного цикла игры"""
         while True:
-            # Отслеживание событий клавиатуры и мыши
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # Заливка фона
-            self.screen.fill(self.settings.bg_color)
-            # Отображение последнего прорисованного экрана
-            pygame.display.flip()
+    def _check_events(self):
+        """Отслеживание событий клавиатуры и мыши"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """Обновление экрана"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # Отображение последнего прорисованного экрана
+        pygame.display.flip()
+
 
 
 if __name__ == '__main__':

e9f4eaca27d71ecd6a013426a8b717fc2580c0f4
Prikusator <squonchi@gmail.com>
2021-07-13 01:51:59 +0300

    Adding ship speed/control settings + refact

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,6 +23,7 @@ class AlienInvasion:
         """Запуск основного цикла игры"""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -30,6 +31,26 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Реакция на нажатие клавиши"""
+        if event.key == pygame.K_RIGHT:
+            # Перемещение корабля вправо
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            # Перемещение корабля влево
+            self.ship.moving_left = True
+
+    def _check_keyup_events(self, event):
+        """Реакция на отпускание алвиши"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         """Обновление экрана"""
--- ship.py
+++ ship.py
@@ -1,17 +1,34 @@
 import pygame
 
+
 class Ship:
     """Класс для управления кораблем"""
+
     def __init__(self, ai_game):
         """Инициализация корабля и задание начальной позиции"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Загрузка изображения корабля
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         # Каждый новый корабль повяляется у нижнего края экрана
         self.rect.midbottom = self.screen_rect.midbottom
+        # Сохранение вещественной координаты центра корабля
+        self.x = float(self.rect.x)
+        # Фллаг перемещения вправо
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Обновление позиции корабля с учетом флага"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        # Обновление позиции корабля
+        self.rect.x = self.x
 
     def blitme(self):
         """Рисует корабль в текущей позиции"""

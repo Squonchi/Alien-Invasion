b794bb4cb5176c1a98576b4f97106d0e59ca6559
Prikusator <squonchi@gmail.com>
2021-08-25 01:26:15 +0300

    Added ships_left and score_images

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -24,8 +25,9 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
-        # Экзэмпляр для хранения игровой статистики
+        # Экзэмпляр для хранения игровой статистики и панели результатов
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -85,6 +87,9 @@ class AlienInvasion:
             self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
+            self.sb.prep_level()
+            self.sb.prep_ships()
 
             # Очистка списков пришельцев и снарядов
             self.aliens.empty()
@@ -143,11 +148,20 @@ class AlienInvasion:
         """Обработка коллизий снарядов с пришельцами"""
         # При обнаружении коллизии удалить снаряд и прешельца
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.aliens_points + len(aliens)
+            self.sb.prep_score()
+            self.sb.check_high_score()
         if not self.aliens:
             # Уничтожение существующих снарядов и создание нового флота
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Увеличение уровня
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _update_aliens(self):
         """Обновление позиций пришельцев"""
@@ -179,6 +193,7 @@ class AlienInvasion:
         if self.stats.ships_left > 0:
             # Уменьшает ship_left
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Очистка списков пришельцев и снарядов
             self.aliens.empty()
@@ -210,6 +225,8 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+        # Вывод информации о счете
+        self.sb.show_score()
         # Кнопка Play
         if not self.stats.game_active:
             self.play_button.draw_button()
--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ class Settings:
         self.ship_limit = 3
         # Параметры снаряда
         self.bullet_speed = 1
-        self.bullet_width = 300
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 10
@@ -22,6 +22,8 @@ class Settings:
 
         # Темп ускорения игры
         self.speedup_scale = 1.1
+        # Темп роста стоимости пришельцев
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -31,12 +33,16 @@ class Settings:
         self.bullet_speed_factor = 3.0
         self.alien_speed_factor = 1.0
         self.fleet_direction = 1
+        # Подсчет очков
+        self.aliens_points = 50
 
     def increase_speed(self):
-        """Увеличивает настройки скорости"""
+        """Увеличивает настройки скорости и стоимости пришельцев"""
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
+
+        self.aliens_points = int(self.aliens_points * self.score_scale)
 
 
 
